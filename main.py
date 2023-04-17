c84ea5fb7815c8477cb86375d68dae3ae3439f85
KisungHong <95329249+KisungHong@users.noreply.github.com>
2023-04-17 18:04:17 +0900

    use many function of pandas

--- main.py
+++ main.py
@@ -15,6 +15,31 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data)
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(data)
+# print(data["temp"])
+#
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+#
+# avg_temp = data["temp"].mean()
+# print(avg_temp)
+#
+# max_temp = data["temp"].max()
+# print(max_temp)
+#
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp
+# monday_temp_F = monday_temp * 9 / 5 + 32
+# print(monday_temp_F)
+
+data_dict = {
+    "students" : ["Amy", "James", "Angela"],
+    "scores" : [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
