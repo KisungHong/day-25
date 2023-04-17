be5076f697cffdba98a759898e98b6d65a9a6dc7
KisungHong <95329249+KisungHong@users.noreply.github.com>
2023-04-17 20:50:53 +0900

    data extract what I want by pandas

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-import pandas
+# import pandas
 
 # data = pandas.read_csv("weather_data.csv")
 # print(data)
@@ -36,10 +36,31 @@ import pandas
 # monday_temp_F = monday_temp * 9 / 5 + 32
 # print(monday_temp_F)
 
+# data_dict = {
+#     "Fur Color" : ["grey", "red", "black"],
+#     "Count" : [2473, 392, 103]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("squirrel_count.csv")
+
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrel = data[data["Primary Fur Color"] == "Gray"]
+print(grey_squirrel)
+grey_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+print(grey_squirrel_count)
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+print(red_squirrel_count)
+print(black_squirrel_count)
+
 data_dict = {
-    "students" : ["Amy", "James", "Angela"],
-    "scores" : [76, 56, 65]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrel_count, red_squirrel_count, black_squirrel_count]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
